34fb175a95285f687fb46836d6020984db9bb27c
Diego Rodrigo <diego-break-@hotmail.com>
2019-11-25 19:38:57 +0100

    clean some comments

--- names.py
+++ names.py
@@ -1,35 +1,3 @@
-'''
-import requests
-
-r = requests.post("https://www.dcode.fr/api/", data={"tool":"word-contraction-generator", "word1":"NORTH KOREA", "word2":"FELIPE II", "word3":""})
-
-d = r.json()["results"]
-print(d)
-		
-def vtoc_idx(s):
-    regex_iter = re.finditer(r'[aeiou][^aeiou]', s.lower())
-    positions = [ i.start() for i in regex_iter]
-	
-def portnameteau(name_list):
-	random.shuffle(name_list)
-	s1 = name_list[0]
-	s2 = name_list[1]
-	end_s1 = vtoc_idx(s1)
-	if end_s1 is None:
-		end_s1 = len(s1)
-	start_s2 = vtoc_idx(s2)
-	if start_s2 is None:
-		start_s2 = 0
-	return (s1[:end_s1] + s2[start_s2:])
-
-if __name__ == '__main__':
-
-	import sys
-	names = sys.argv[1:3]
-	print portnameteau(names)
-
-'''
-
 import re
 import random
 import sys
